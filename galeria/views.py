bbb796123efd007aa493e45ea9418e26cc8ff35b
thiessen2003 <thiessengabriel@gmail.com>
2023-05-05 10:58:02 -0300

    Projeto alura-space

---
+++ galeria/views.py
@@ -0,0 +1,6 @@
+#Responsável por renderizar os conteúdos
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<h1>Alura Space</h1><p>Bem-vindo ao espaço</p>')
